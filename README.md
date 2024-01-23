20d96789bf0fa624ff6382496c4a7e7606b22bd3
Joshua <Joshua.soto@live.com>
2024-01-22 18:23:48 -0600

    added about me section

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # joshsportfolio
 
 ## Description
-
+This will be my professional portfolio made using back-end. This will be an updated version of the portfolio i made in week 2 of the Coding Boot Camp.
 ## Table of Contents 
 
 ## Installation
---
+++ src/components/About.js
@@ -0,0 +1,22 @@
+import React from "react"; 
+
+function About() {
+  return (
+  <div>
+<p className="content is strong">Diehard Philadelphia Eagles Fan and soon to be a Full Stack Web Dev!</p>
+ <hr/>
+  <img className="my-pic"></img>
+  <p className="content is-medium mt-4">
+    Welcome to my Portfolio! My name is Josh Soto. I am currently a produce manager employed at Whole Foods Market. I'm looking to become a full stack web dev one day really soon. Thanks for stopping bye for a visit! Go BIRDS!
+  </p>
+
+<p className="content is-weak">
+    I enjoy trying new things and learning new skills. 
+</p>
+
+
+  </div>
+ );
+}
+
+export default About;
