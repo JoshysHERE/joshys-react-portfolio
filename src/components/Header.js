6340328c1deca2d4023d3ec33b2120ba0922b881
Joshua <Joshua.soto@live.com>
2024-01-23 18:37:33 -0600

    hopefully got the nav bar working

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,7 +18,34 @@ function Header() {
                     return<About />;
         } 
       };
-    }
+    
 
+
+return (
+    <div>
+      <nav className='navbar'>
+        <div className='navbar-brand'>
+         <a 
+         className='navbar-item'
+         rel='noreferrer'
+         target='_blank'
+         href="https://github.com/JoshysHERE">
+         </a>
+          <span className='content is-bold'>Joshua Soto</span>
+        
+        </div>
+      </nav>
+      
+      <Navigation
+        currentPage={currentPage}
+        handlePageChange={handlePageChange}
+      />
+       
+      <main>
+        <div>{renderPage(currentPage)}</div>
+      </main>
+    </div>
+  );
+}
 
 export default Header;
